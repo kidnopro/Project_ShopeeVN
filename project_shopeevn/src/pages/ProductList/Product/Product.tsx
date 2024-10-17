079e873d40afa5999521da35caddc3f7794f368a
kidnopro <hoanganhfullstack@gmail.com>
2024-10-17 20:55:42 +0700

    Format lại giá tiền và số lượt bán cho giống Việt Nam

--- project_shopeevn/src/pages/ProductList/Product/Product.tsx
+++ project_shopeevn/src/pages/ProductList/Product/Product.tsx
@@ -1,28 +1,37 @@
 import { Link } from "react-router-dom";
+import { Product as ProductType } from "../../../types/product.type";
+import {
+  formatCurrency,
+  formatNumberToSocialStyle,
+} from "../../../utils/utils";
 
-export default function Product() {
+interface Props {
+  product: ProductType;
+}
+
+export default function Product({ product }: Props) {
   return (
     <Link to="/">
       <div className="bg-white shadow rounded-sm hover: translate-y-[-0.0625rem] hover:shadow-md duration-100 transition-transform">
         <div className="w-full pt-[100%] relative">
           <img
-            src="https://images2.thanhnien.vn/528068263637045248/2023/4/23/edit-truc-anh-16822518118551137084698.png"
-            alt=""
+            src={product.image}
+            alt={product.name}
             className="absolute top-0 left-0 bg-white w-full h-full object-cover"
           />
         </div>
         <div className="p-2 overflow-hidden">
           <div className="min-h-[1.75rem] line-clamp-2 text-sm">
-            hàng hiếu iêu xịn rất nhiều sán phẩm lỗi được bán ra, mong quý khách
-            ủng hố ản phẩm nhiều
+            {product.name}
           </div>
           <div className="flex items-center mt-3">
             <div className="line-through max-w-[50%] text-gray-400 truncate">
-              đ 2000
+              <span className="">đ</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className="text-orange-500 truncate ml-1">
               <span className="text">đ</span>
-              <span>2300</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className="mt-3 flex items-center justify-end">
@@ -30,9 +39,27 @@ export default function Product() {
               <div className="relative">
                 <div
                   className="absolute top-0 left-0 h-full overflow-hidden"
-                  style={{ width: "50%" }}>
-                      
+                  style={{ width: "50%" }}
+                >
+                  <svg
+                    enableBackground="new 0 0 15 15"
+                    viewBox="0 0 15 15"
+                    x={0}
+                    y={0}
+                    className="w-3 h-3 fill-yellow-300 text-yellow-300"
+                  >
+                    <polygon
+                      points="7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
                 </div>
+              </div>
+              <div className="ml-2 text-sm">
+                <span>{formatNumberToSocialStyle(product.sold)}</span>
+                <span className="ml-1">Đã bán</span>
               </div>
             </div>
           </div>
--- project_shopeevn/src/types/auth.type.ts
+++ project_shopeevn/src/types/auth.type.ts
@@ -1,7 +1,7 @@
 import { User } from "./user.type";
 import { SuccessReponse } from "./utils.type";
 
-export type AuthReponse = SuccessReponse<{
+export type AuthResponse = SuccessReponse<{
   access_token: string;
   expires: string;
   user: User;
